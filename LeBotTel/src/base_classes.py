a473a5470db640ae0beb3785a4fab82ebd937f7f
Tobias Weiss <t.weiss@oth-aw.de>
2023-12-07 14:35:37 +0100

    feat: init LeBotTel package and add telegram bot base class

---
+++ LeBotTel/src/base_classes.py
@@ -0,0 +1,36 @@
+import requests
+
+class ChatIdError(Exception):
+    pass
+
+
+class BaseTelegramBot:
+    def __init__(self, token:str, chat_id:str=None) -> None:
+        self.token = token
+        self.chat_id = chat_id
+        self.base_url = f'https://api.telegram.org/bot{self.token}/'
+
+    def get_chat_id(self) -> None:
+        url = f'{self.base_url}getUpdates'
+        response = requests.get(url)
+        data = response.json()
+        try:
+            self.chat_id = data['result'][0]['message']['chat']['id']
+        except Exception as e:
+            raise ChatIdError('chat_id not found') from e
+
+    def send_message(self, message:str) -> dict:
+        if self.chat_id is None:
+            raise ChatIdError('chat_id is not set')
+        url = f'{self.base_url}sendMessage?chat_id={self.chat_id}&text={message}'
+        response = requests.get(url)
+        return response.json()
+    
+    def send_image(self, image: bytes) -> dict:
+        if self.chat_id is None:
+            raise ChatIdError('chat_id is not set')
+        url = f'{self.base_url}sendPhoto'
+        files = {'photo': ("image.png", image, "image/png")}
+        data = {'chat_id' : self.chat_id}
+        response = requests.post(url, files=files, data=data)
+        return response.json()
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="LeBotTel",                        # This is the name of the package
+    version="0.1.0",                        # The initial release version
+    author="Antonio Vidos, Tobias Lettner, Tobias Weiß, Uwe Kölbel",
+    author_email="",                        # TODO: Add the email address here
+    description="A simple Telegram Bot",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.9',                # Minimum version requirement of the package
+    py_modules=["LeBotTel"],                # Name of the python package
+    package_dir={'':'LeBotTel/src'},        # Directory of the source code of the package
+    install_requires=required               # Install other dependencies if any
+)
