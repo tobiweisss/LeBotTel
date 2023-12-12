875f24e9de9474e6fcb655592b8b135837c9f247
Tobias Weiss <t.weiss@oth-aw.de>
2023-12-12 22:28:40 +0100

    fix: move source code from LeBotTel/LeBotTel/ to LeBotTel/

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setuptools.setup(
     ],  # Information to filter the project on PyPi website
     python_requires=">=3.10",  # Minimum version requirement of the package
     py_modules=["LeBotTel"],  # Name of the python package
-    package_dir={"": "LeBotTel"},  # Directory of the source code of the package
     install_requires=[
         "requests~=2.31.0",  # Add any additional packages here
     ],  # Install other dependencies if any
